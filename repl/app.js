4f5bdfb7011ec28b8c320dd37dcc0bb323fcbdb7
kachibee <41226476+kachibee@users.noreply.github.com>
2021-11-30 13:26:39 +0800

    Update app.js

--- repl/app.js
+++ repl/app.js
@@ -67,9 +67,9 @@ class App {
         this.#talentExtend = localStorage.talentExtend;
         await this.#life.initial();
         this.output(`\rLoading Complete.
-人生重开模拟器
-这垃圾人生一秒也不想待了
-\n🎉键入 \x1B[4m/remake\x1B[24m 开始游戏`,
+人生重启模拟器
+这人生一秒也不想待了
+\n🎉键入 \x1B[4m/remake\x1B[24m 开始重启`,
             true
         );
         $$on('achievement', ({name})=>this.output(`
